01bbc50674930116b92b7fe02ac16028a3279b88
Johan <ferrerasjohan@gmail.com>
2015-08-03 10:57:18 -0400

    created app.yaml and other files

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import jinja2
 import os
 import webapp2
 from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class AnswerEntry(ndb.Model):
+    db_food = BooleanProperty(required=True)
+    
